00a1ef2d3a76126432ecedb3cc99abb9913bc7a6
ThalisonBotz <thalison.botelho.silva@gmail.com>
2022-12-12 20:55:06 -0300

    Update Repository

--- Backend/src/main/java/com/onbox/userweb/controller/ProdutoControler.java
+++ Backend/src/main/java/com/onbox/userweb/controller/ProdutoControler.java
@@ -37,7 +37,7 @@ public class ProdutoControler {
 
 
     //busca por nome
-    //localhost:8080/produto/find?teste
+    //localhost:8080/produto/find?nome=nome salvo no banco
     @GetMapping(path = "/find")
     public ResponseEntity<List<Produto>> findByNome(@RequestParam String nome) {
         return ResponseEntity.ok(produtoService.findByNome(nome));
